a53c5ef08f25b5994dae6d89ee82e84499468b1a
Ikaro Castro <nunesikaro@gmail.com>
2026-05-28 21:20:14 -0300

    modifiquei o codigo OperadoresLogico

--- src/operadoresAtribuicaoELogicos/OperadoresLogicos.java
+++ src/operadoresAtribuicaoELogicos/OperadoresLogicos.java
@@ -5,15 +5,26 @@ public class OperadoresLogicos {
         var scanner = new Scanner(System.in);
         System.out.println("Qual é sua idade? ");
         var age = scanner.nextInt();
-        var canDrive = age >= 18;
+        System.out.println("Voce é emancipado? ");
+        var isEmancipated = scanner.nextBoolean();
+        var canDrive = age >= 18 || isEmancipated;
         System.out.printf("Voce pode dirigir? (%s) \n", canDrive);
-        if (age > 18) {
+        if (age > 18 || isEmancipated) {
             System.out.println("Voce pode dirigir!");
         } else if (age < 18) {
-            System.out.println("Voce pode não dirigir!");
+            System.out.println("Voce não pode dirigir!");
         }
     }
 }
-
+// parei no video no minuto 18:23
 //isRigth é bool para false se errar! e true se acerta!
 // isWrong é bool invertido
+
+//operadores
+
+// > significa (maior que)
+// < significa (menor que)
+// == significa (igual à)
+// != significa (diferente de)
+// >= significa (maior ou igual à)
+// <= significa (menor ou igual à)
